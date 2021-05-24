0e2d29836ca47072090bb2457b2d361740d4e0a4
gosaotru <69381915+gosatoru@users.noreply.github.com>
2021-05-25 02:52:58 +0800

    Update jobs.vue

--- jobsite/views/jobs/jobs.vue
+++ jobsite/views/jobs/jobs.vue
@@ -1,46 +1,52 @@
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300;400&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Poppins:200, 300, 400, 500, 600, 700, 800, 900&display=swap');
-
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300;400&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+	@import url('https://fonts.googleapis.com/css?family=Poppins:200, 300, 400, 500, 600, 700, 800, 900&display=swap');
 </style>
 
 
 <template>
   <h1>Jobs</h1>
-	<div v-for="job in jobs" :key="job.id" class="job">
-		<router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-		  <h2> {{ job.title }} </h2>
-		</router-link>
+	<div v-if="jobs.length">
+		<div v-for="job in jobs" :key="job.id" class="job">
+			<router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+				<h2> {{ job.title }} </h2>
+			</router-link>
+		</div>
 	</div>
+	<div v-else>
+		<p>Loading...</p>
+	</div>
+
 </template>
 
 <script>
 export default {
   data() {
 		return {
-			jobs: [
-				{ title: 'UX Designer', id: 1, details: 'Lorem Ipsum'},
-				{ title: 'Web Developer', id: 2, details: 'Lorem Ipsum'},
-				{ title: 'Vue Developer', id: 3, details: 'Lorem Ipsum'}
-			]
+			jobs: []
 		}
+	},
+	mounted() {
+		fetch('http://localhost:3000/jobs')
+			.then(res => res.json())
+			.then(data => this.jobs = data)
+			.catch(err => console.log(err.message))
 	}
 }
 </script>
 
 <style>
 .job h2 {
-  color: #395066;
-	font-family: 'Open Sans', sans-serif;
-	text-decoration: none;
-	background: #f4f4f4;
-  padding: 20px;
-  border-radius: 10px;
-  margin: 10px auto;
-  max-width: 240px;
-  cursor: pointer;
+  	color: #395066;
+		font-family: 'Open Sans', sans-serif;
+		text-decoration: none;
+		background: #f4f4f4;
+  	padding: 20px;
+  	border-radius: 10px;
+  	margin: 10px auto;
+  	max-width: 240px;
+  	cursor: pointer;
 }
 .job h2:hover {
   background: #ddd;

acddd2c63517c057a0161a8d89fae26c2822937f
gosaotru <69381915+gosatoru@users.noreply.github.com>
2021-05-25 00:36:35 +0800

    jobdetails.vue

---
+++ jobsite/views/jobs/jobdetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Job Details</h1>
+  <p>The job ID is {{ id }}</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+  <p>incididunt ut labore et dolore magna aliqua purus faucibus .</p>
+  <p>Ornare suspendisse sed. Neque sodales ut etiam sit amet nisl purus in. Eu facilisis sed odio morbi.</p>
+</template>
+
+<script>
+export default {
+	props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //       }
+  //   }
+}
+</script>
+
+<style>
+
+</style>

e2b1ddd4c855a0b576b7d67cfc96578a67aa1e86
gosaotru <69381915+gosatoru@users.noreply.github.com>
2021-06-01 00:26:19 +0800

    Create main.js

---
+++ Vue-TODO/data/main.js
@@ -0,0 +1,28 @@
+{
+  "projects": [
+    {
+      "id": 1,
+      "title": "Lorem ispum dolor sit amet.",
+      "details": "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "complete": true
+    },
+    {
+      "id": 2,
+      "title": "What is Lorem Ipsum?",
+      "details": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+      "complete": false
+    },
+    {
+      "title": "Lorem Ipsum is a dummy text.",
+      "details": "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      "complete": true,
+      "id": 3
+    },
+    {
+      "title": "Tellus mauris a diam maecenas",
+      "details": "Sed enim ut sem adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Pretium nibh ipsum consequat nisl vel pretium. Odio morbi quis commodo odio aenean sed adipiscing diam. Suscipit tellus mauris a diam maecenas sed enim. Pellentesque pulvinar pellentesque habitant morbi. Massa sed elementum tempus egestas sed. Dolor sit amet consectetur adipiscing elit. Vel eros donec ac odio tempor orci. Nulla pellentesque dignissim enim sit amet. Faucibus turpis in eu mi bibendum neque egestas congue.",
+      "complete": false,
+      "id": 4
+    }
+  ]
+}

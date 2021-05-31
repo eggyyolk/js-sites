fae8620539c759a20b68434b247a4e17cfc693a6
gosaotru <69381915+gosatoru@users.noreply.github.com>
2021-06-01 00:19:19 +0800

    Create App.vue

---
+++ Vue-TODO/App.vue
@@ -0,0 +1,32 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+</style>
+
+<template>
+  <Navbar />
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+body {
+  background: #f3f7fa;
+}
+#app {
+  font-family:'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+
+</style>

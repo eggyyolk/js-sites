a0ae81de22dd3b367730efb46def2634215a3318
eggyolk <69381915+eggyyolk@users.noreply.github.com>
2021-06-09 23:32:30 +0800

    Create App.vue

---
+++ compositionapi/App.vue
@@ -0,0 +1,31 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+</style>
+
+
+<template>
+  <Navbar />
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #070707;
+  max-width: 40%;
+  margin-left: auto;
+  margin-right: auto;
+  width: 34em
+}
+</style>

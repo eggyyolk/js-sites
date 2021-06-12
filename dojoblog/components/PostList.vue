32c65859daaa1db3494e606f3cbdcd48c2c72be5
eggyolk <69381915+eggyyolk@users.noreply.github.com>
2021-06-12 17:10:34 +0800

    Create PostList.vue

---
+++ dojoblog/components/PostList.vue
@@ -0,0 +1,25 @@
+<template class="main">
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+
+export default {
+  props: ['posts'],
+  components: { SinglePost },
+  setup(props) {
+    onMounted(() => console.log('mounted'))
+    onUnmounted(() => console.log('unmounted'))
+    onUpdated(() => console.log('updated'))
+  }
+}
+</script>
+
+<style>
+</style>

68e696e3ecb12b90cf5e99839c662a4973532d97
eggyolk <69381915+eggyyolk@users.noreply.github.com>
2021-06-12 17:07:49 +0800

    Create Home.vue

---
+++ dojoblog/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div v-if="error"> {{ error }} </div>
+    <div v-if="posts.length" >
+      <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
+    </div>
+    <div v-else>
+      <Spinner />
+  </div>
+  </div>
+</template>
+
+<script>
+import Spinner from '../components/Spinner.vue'
+import PostList from '../components/PostList.vue'
+import getPosts from '/composables/getPosts'
+import TagCloud from '../components/TagCloud.vue'
+
+export default {
+  name: 'Home',
+  components: { PostList, Spinner, TagCloud },
+  setup() {
+    const{ posts, error, load } = getPosts()
+
+    load()
+
+    return { posts, error }
+  }
+}
+</script>
+
+<style>
+ .home {
+   max-width: 1200px;
+   margin: 0 auto;
+   padding: 10 auto;
+ }
+</style>

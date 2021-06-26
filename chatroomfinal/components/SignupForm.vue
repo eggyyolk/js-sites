1ca7e19693c01d31c5c76fd6bebe674ce8ade983
gosatoru <69381915+gosatoru@users.noreply.github.com>
2021-06-26 22:37:49 +0800

    Create SignupForm.vue

---
+++ chatroomfinal/components/SignupForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="Display Name" v-model="displayName">
+      <input type="email" required placeholder="Email" v-model="email">
+        <input type="password" required placeholder="Password" v-model="password"> 
+				<div class="error">{{ error }}</div>
+        <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import useSignup from '../../src/composables/useSignup'
+
+export default {
+	setup(props, context) {
+		const { error, signup } = useSignup()
+
+		const displayName = ref('')
+		const email = ref('')
+		const password = ref('')
+		
+		const handleSubmit = async () => {
+			await signup(email.value, password.value, displayName.value)
+			if (!error.value) {
+				context.emit('signup')
+			}
+	
+		}
+
+		return { displayName, password, email, handleSubmit, error }
+	}
+}
+</script>
+
+<style>
+
+</style>

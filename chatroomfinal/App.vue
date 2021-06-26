e2f8055b74c39e4bd46e30fb698eb94619522be1
gosatoru <69381915+gosatoru@users.noreply.github.com>
2021-06-26 22:26:53 +0800

    App.vue

---
+++ chatroomfinal/App.vue
@@ -0,0 +1,16 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');</style>
+
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1a1a1a;
+}
+</style>
